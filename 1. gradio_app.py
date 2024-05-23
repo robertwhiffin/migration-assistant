f6dd5de6ad51220f32d1c2cf5031cb9c7b0da12b
robertwhiffin <robertwhiffin>
2024-05-23 15:22:19 +0000

    add in chatbot functionality and accordion drop downs and advanced settings

--- 1. gradio_app.py
+++ 1. gradio_app.py
@@ -24,7 +24,7 @@ intent_table = os.environ["INTENT_TABLE"]
 # details for connecting to the llm endpoint
 
 # the URL of the serving endpoint
-MODEL_SERVING_ENDPOINT_URL = f"https://{DATABRICKS_HOST}serving-endpoints"
+MODEL_SERVING_ENDPOINT_URL = f"https://{DATABRICKS_HOST}/serving-endpoints"
 
 client = OpenAI(
   api_key=DATABRICKS_TOKEN,
@@ -67,8 +67,8 @@ def get_table_metadata(input_dict):
 
 ################################################################################
 ################################################################################
-# this is called to actually send a request and receive response from the llm endpoint.
-def ask_llm(metadata_prompt, no_metadata_prompt, sql_query):
+# use this to build to the initial prompt to get the intent
+def build_table_metadata(sql_query):
     # get tables and columns
     table_info = parse_sql(sql_query, 'tsql')
     # get table and column metadata
@@ -80,10 +80,20 @@ def ask_llm(metadata_prompt, no_metadata_prompt, sql_query):
     if table_metadata != []:
         # join up the metadata into a single string to add into the prompt
         table_column_descriptions = "\n\n ".join(table_metadata)
+        return table_column_descriptions
+    else:
+        return None
+
+################################################################################
+################################################################################
+# this is called to actually send a request and receive response from the llm endpoint.
+def ask_llm(metadata_prompt, no_metadata_prompt, sql_query):
+    table_descriptions = build_table_metadata(sql_query)
+    if table_descriptions:
         # set the system prompt
         system_prompt = metadata_prompt
         # build the query prompt by adding code and metadata descriptions
-        query_prompt = f"This is the SQL code: {sql_query}. \n\n{table_column_descriptions}"
+        query_prompt = f"This is the SQL code: {sql_query}. \n\n{table_descriptions}"
     else:
         system_prompt = no_metadata_prompt
         # build the query prompt by adding code and metadata descriptions
@@ -98,9 +108,44 @@ def ask_llm(metadata_prompt, no_metadata_prompt, sql_query):
         model=os.environ["SERVED_MODEL_NAME"],
         max_tokens=int(os.environ["MAX_TOKENS"])
     )
-            
+
+    # helpful for debugging -show the query sent to the LLM        
     #return [chat_completion.choices[0].message.content, query_prompt]
+    # this is the return without the chat interface
+    #return chat_completion.choices[0].message.content
+    # this is the return for the chatbot - empty string to fill in the msg, list of lists for the chatbot
+    return "", [[query_prompt, chat_completion.choices[0].message.content]]
+
+def call_llm_for_chat(chat_history, query, metadata_prompt, no_metadata_prompt, sql_query):
+    table_descriptions = build_table_metadata(sql_query)
+    if table_descriptions:
+        # set the system prompt
+        system_prompt = metadata_prompt
+        # build the query prompt by adding code and metadata descriptions
+        query_prompt = f"This is the SQL code: {sql_query}. \n\n{table_descriptions}"
+    else:
+        system_prompt = no_metadata_prompt
+        # build the query prompt by adding code and metadata descriptions
+        query_prompt = f"This is the SQL code: {sql_query}"
+    system_prompt = "You are a chatbot which helps users explain the intent of their code."
+    messages=[
+        {"role": "system", "content": system_prompt}
+        ] 
+    for q, a in chat_history:
+      messages.extend(
+        [{"role": "user",  "content": q}
+        ,{"role": "assistant",  "content": a}]
+        )
+    messages.append(
+        {"role": "user",  "content": query})
+    
+    chat_completion = client.chat.completions.create(
+        messages=messages,
+        model=os.environ["SERVED_MODEL_NAME"],
+        max_tokens=int(os.environ["MAX_TOKENS"])
+    )
     return chat_completion.choices[0].message.content
+
 ################################################################################
 ################################################################################
 # this writes the code & intent into the lookup table
@@ -114,7 +159,8 @@ def save_intent(code, intent):
 ################################################################################
 ################################################################################
 # this does a look up on the vector store to find the most similar code based on the intent
-def get_similar_code(intent):    
+def get_similar_code(chat_history):
+    intent=chat_history[-1][1]    
     results = vsc.get_index(VECTOR_SEARCH_ENDPOINT_NAME, vs_index_fullname).similarity_search(
     query_text=intent,
     columns=["code", "intent"],
@@ -128,14 +174,18 @@ def get_similar_code(intent):
 # this is the app UI. it uses gradio blocks https://www.gradio.app/docs/gradio/blocks
 # each gr.{} call adds a new element to UI, top to bottom. 
 with gr.Blocks(theme=gr.themes.Soft()) as demo:
-    # title with DfE image
+    # title with Databricks image
     gr.Markdown("""<img align="right" src="https://asset.brandfetch.io/idSUrLOWbH/idm22kWNaH.png" alt="logo" width="120">
 
-## A context aware migration assistant for explaining the intent of SQL code and converting from T-SQL to Spark SQL
+## A context aware migration assistant for explaining the intent of SQL code and conversion to Spark SQL
 
 #### This demo relies on the tables and columns referenced in the SQL query being present in Unity Catalogue and having their table comments and column comments populated. For the purpose of the demo, this was generated using the Databricks AI Generated Comments tool. 
 
 """)
+    
+################################################################################
+#### ADVANCED OPTIONS PANE
+################################################################################
     with gr.Accordion(label="Advanced Settings", open=False):
         with gr.Row():
             # select SQL flavour
@@ -169,70 +219,160 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
         llm_sys_prompt_metadata = gr.Textbox(
             label="System prompt for LLM to generate code intent if table metadata present."
             ,value="""
-                Your job is to explain the intent of a SQL query. You are provided with the SQL Code and a summary of the information contained within the tables queried, and details about which columns are used from which table in the query. From the information about the tables and columns, you will infer what the query is intending to do.\n 
-                """
+                Your job is to explain the intent of a SQL query. You are provided with the SQL Code and a summary of the information contained within the tables queried, and details about which columns are used from which table in the query. From the information about the tables and columns, you will infer what the query is intending to do.
+                """.strip()
             )
         llm_sys_prompt_no_metadata = gr.Textbox(
             label="System prompt for LLM to generate code intent if table metadata absent."
             ,value="""
                 Your job is to explain the intent of this SQL code.
-                """
+                """.strip()
             )
-
+################################################################################
+#### TRANSLATION PANE
+################################################################################
     # subheader
-    gr.Markdown(""" ### Input your T-SQL code here for automatic translation to Spark-SQL and use AI to generate a statement of intent for the code's purpose.
-                """)
-    # input box for SQL code with nice formatting
-    input_code = gr.Code(
-            label="Input SQL"
-            ,language='sql'
+    with gr.Accordion(label="Translation Pane", open=True):
+        gr.Markdown(""" ### Input your T-SQL code here for automatic translation to Spark-SQL and use AI to generate a statement of intent for the code's purpose."""
+                    )
+
+        # a button labelled translate
+        translate_button = gr.Button("Translate") 
+        with gr.Row():
+
+            with gr.Column():
+                gr.Markdown(
+                    """ ### Input your T-SQL code here for translation to Spark-SQL."""
+                    )
+                
+                # input box for SQL code with nice formatting
+                input_code = gr.Code(
+                        label="Input SQL"
+                        ,language='sql-msSQL'
+                        ,value="""SELECT
+  c.[country_name],
+  AVG([dep_count]) AS average_dependents
+FROM
+  (
+    SELECT
+      e.[employee_id]
+      ,e.[department_id]
+      ,COUNT(d.[dependent_id]) AS dep_count
+    FROM
+      [robert_whiffin].[dfe_code_assistant].[employees] e
+      LEFT JOIN [robert_whiffin].[dfe_code_assistant].[dependents] d ON e.[employee_id] = d.[employee_id]
+    GROUP BY
+      e.[employee_id]
+      ,e.[department_id]
+  ) AS subquery
+  JOIN [robert_whiffin].[dfe_code_assistant].[departments] dep ON subquery.[department_id] = dep.[department_id]
+  JOIN [robert_whiffin].[dfe_code_assistant].[locations] l ON dep.[location_id] = l.[location_id]
+  JOIN [robert_whiffin].[dfe_code_assistant].[countries] c ON l.[country_id] = c.[country_id]
+GROUP BY
+  c.[country_name]
+ORDER BY
+  c.[country_name]"""
+                        )
+            # the input code box gets updated when a user changes a setting in the Advanced section
+                sql_flavour.input(update_input_code_box, sql_flavour, input_code)
+            
+            with gr.Column():
+                # divider subheader
+                gr.Markdown(""" ### Your Code Translated to Spark-SQL""")
+                # output box of the T-SQL translated to Spark SQL
+                translated = gr.Code(
+                    label="Your code translated to Spark SQL"
+                    ,language="sql-sparkSQL"
+                    )
+        translate_button.click(fn=sqlglot_transpilation, inputs=input_code, outputs=translated)
+
+################################################################################
+#### AI GENERATED INTENT PANE
+################################################################################
+    # divider subheader
+
+    with gr.Accordion(label="Intent Pane", open=True):
+        gr.Markdown(""" ## AI generated intent of what your code aims to do. 
+                    
+                    Intent is determined by an LLM which uses the code and table & column metadata. 
+
+                    ***If the intent is incorrect, please edit***. Once you are happy that the description is correct, please click the button below to save the intent. This will help the Department by making it easier to identify duplication of what people are doing. 
+                    """)
+        # a box to give the LLM generated intent of the code. This is editable as well. 
+        explain_button = gr.Button("Explain code intent using AI.")
+        explained = gr.Textbox(label="AI generated intent of your code.", visible=False)
+
+        chatbot = gr.Chatbot(
+            label = "AI Chatbot for Intent Extraction"
+            ,height="70%"
             )
-    # the input code box gets updated when a user changes a setting in the Advanced section
-    sql_flavour.input(update_input_code_box, sql_flavour, input_code)
-    # a button labelled run
-    btn = gr.Button("Run")
-    # divider subheader
-    gr.Markdown(""" ## Your Code Translated to Spark-SQL
-                
-                Translation is deterministic and done by SQL Glot
-                """)
-    # output box of the T-SQL translated to Spark SQL
-    translated = gr.Code(
-        label="Your code translated to Spark SQL"
-        ,language="sql-sparkSQL"
-        )
-    # divider subheader
-    gr.Markdown(""" ## AI generated intent of what your code aims to do. 
-                
-                Intent is determined by an LLM which uses the code and table & column metadata. 
+        
+        msg = gr.Textbox(label="Instruction")
+        clear = gr.ClearButton([msg, chatbot])
 
-                ***If the intent is incorrect, please edit***. Once you are happy that the description is correct, please click the button below to save the intent. This will help the Department by making it easier to identify duplication of what people are doing. 
-                """)
-    # a box to give the LLM generated intent of the code. This is editable as well. 
-    explained = gr.Textbox(label="AI generated intent of your code.")
+        def user(user_message, history):
+            return "", history + [[user_message, None]]
 
-    # divider subheader
-    gr.Markdown(""" ## Similar code 
-                
-                This code is thought to be similar to what you are doing, based on comparing the intent of your code with the intent of this code.
-                """)    
-    # a button
-    find_similar_code=gr.Button("Find similar code")
-    # a row with an code and text box to show the similar code
-    with gr.Row():
-        similar_code = gr.Code(
-            label="Similar code to yours."
-            ,language="sql-sparkSQL"
+        def respond(chat_history, message, metadata_prompt, no_metadata_prompt, sql_query ):                
+            bot_message = call_llm_for_chat(chat_history, message, metadata_prompt, no_metadata_prompt, sql_query)
+            chat_history.append([message, bot_message])
+            return "", chat_history
+        
+        explain_button.click(
+            fn=ask_llm
+            , inputs=[
+                llm_sys_prompt_metadata
+                , llm_sys_prompt_no_metadata
+                , input_code
+                ]
+            , outputs=[msg, chatbot]
             )
-        similar_intent = gr.Textbox(label="The similar codes intent.")
+        # msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+        #     respond, chatbot, chatbot
+        # )
+        msg.submit(
+            fn=respond
+            ,inputs = [
+                chatbot
+                , msg
+                , llm_sys_prompt_metadata
+                , llm_sys_prompt_no_metadata
+                , input_code
+                ],
+            outputs= [msg, chatbot])
+        clear.click(lambda: None, None, chatbot, queue=False)
 
-    # a button
-    submit = gr.Button("Save code and intent")
 
-    # assign actions to buttons when clicked.
-    btn.click(fn=sqlglot_transpilation, inputs=input_code, outputs=translated)
-    btn.click(fn=ask_llm, inputs=[llm_sys_prompt_metadata, llm_sys_prompt_no_metadata, input_code], outputs=explained)
-    find_similar_code.click(get_similar_code, inputs=explained, outputs=[similar_code, similar_intent])
+
+
+################################################################################
+#### SIMILAR CODE PANE
+################################################################################
+    # divider subheader
+
+    with gr.Accordion(label="Similar Code Pane", open=True):
+        gr.Markdown(""" ## Similar code 
+                    
+                    This code is thought to be similar to what you are doing, based on comparing the intent of your code with the intent of this code.
+                    """)    
+        # a button
+        find_similar_code=gr.Button("Find similar code")
+        # a row with an code and text box to show the similar code
+        with gr.Row():
+            similar_code = gr.Code(
+                label="Similar code to yours."
+                ,language="sql-sparkSQL"
+                )
+            similar_intent = gr.Textbox(label="The similar codes intent.")
+
+        # a button
+        submit = gr.Button("Save code and intent")
+
+        # assign actions to buttons when clicked.
+    find_similar_code.click(
+        fn=get_similar_code
+        , inputs=chatbot
+        , outputs=[similar_code, similar_intent])
     
     submit.click(save_intent, inputs=[input_code, explained])
 
