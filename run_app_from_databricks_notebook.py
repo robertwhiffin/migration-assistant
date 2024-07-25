2b18ddc190e5a90f9b587a83c9c5e0aa10fb779a
robert.whiffin <robert.whiffin@databricks.com>
2024-07-25 19:21:54 +0100

    update run on databricks notebook

--- run_app_from_databricks_notebook.py
+++ run_app_from_databricks_notebook.py
@@ -1,24 +1,16 @@
 # Databricks notebook source
-#pip install -r "requirements.txt"
+pip install -r "requirements.txt"
+
+# COMMAND ----------
+pip install dbtunnel==0.14.6
 
 # COMMAND ----------
 
-#dbutils.library.restartPython()
+dbutils.library.restartPython()
 
 # COMMAND ----------
 
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
-
-# COMMAND ----------
-
-from utils.configloader import ConfigLoader
-cl = ConfigLoader()
-cl.read_yaml_to_env("config.yaml")
-
-# COMMAND ----------
-
-from dbtunnel import dbtunnel
-dbtunnel.kill_port(8080)
-app='././gradio_app.py'
-dbtunnel.gradio(path=app).run()
+from utils.initialsetup import setup_gamma
+from utils.runindatabricks import run_app
+setup_gamma()
+run_app()
